f410768ffa98b268acd3ef6447634eebb51e6c2c
21-manic-002g <21-manic-002g@21-manic-002>
2024-03-26 15:51:47 +0900

    Beakjoon N 찍기 문제

---
+++ Beakjoon/src/q2741/Main.java
@@ -0,0 +1,18 @@
+package q2741;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+
+		//System.out.println(N);
+		int m =1;
+		while (m <= N) {
+			System.out.println(m++);
+		}
+	}			
+}
